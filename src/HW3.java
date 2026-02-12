4284ac3a386b6a439190a94bab3d920eedd65a05
Alexander Doyle <57422635+Suave101@users.noreply.github.com>
2026-02-12 14:48:57 -0500

    Update Test Cases

--- src/HW3.java
+++ src/HW3.java
@@ -1,24 +1,17 @@
 /*
 
-  Author:
-  Email:
-  Course:
-  Section:
-  Description of this file:
+  Author: Alexander Doyle
+  Email: adoyle2025@my.fit.edu
+  Course: Data Structures and Algorithms
+  Section: Section E2/E3
+  Description of this file: Creates a tree to make query to for the Olympics
 
  */
 
 public class HW3
 {
-
-    /*
-      Description of each method, including parameters 
-    */
-    public static void main(String[] args)
-    {
-	/* description of variables */
-
-	/* description of each block (around 5-10 lines) of instructions */
+    public static void main(String[] args) {
+        
     }
 
 }
